415fcfd917173ca78254ff678c2ae1438f0b40a5
Tyler Arthur <tarthur@Lenovo-IdeaPad-FLEX-15IWL-23c919ee.localdomain>
2023-08-22 08:36:32 -0700

    Added session link generation

--- script.js
+++ script.js
@@ -1,56 +1,109 @@
-// Initialize PeerJS
-const peer = new Peer();
+const peer = new Peer(); // Create a new Peer instance
+const connectedPeers = {};
 
-peer.on('open', (peerId) => {
-    console.log('My peer ID is: ' + peerId);
+// Display your Peer ID
+peer.on('open', (id) => {
+    document.getElementById('peerId').textContent = id;
+});
 
-    // Establish connection
-    const connections = {};
+// Handle errors
+peer.on('error', (err) => {
+    console.error('PeerJS Error:', err);
+});
 
-    // Listen for incoming connections
-    peer.on('connection', (connection) => {
-        connections[connection.peer] = connection;
-        connection.on('data', (data) => {
-            displayMessage(connection.peer, data);
-        });
+// Generate a session link
+document.getElementById('generateLink').addEventListener('click', () => {
+    const sessionLink = `${window.location.href}?peer=${peer.id}`;
+    document.getElementById('sessionLink').value = sessionLink;
+});
+
+// Parse the URL for peer ID
+function getPeerIdFromURL() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get('peer');
+}
+
+// Connect to another peer
+function connectToPeer(otherPeerId) {
+    const connection = peer.connect(otherPeerId);
+
+    // Store the connected peer
+    connectedPeers[otherPeerId] = connection;
+
+    console.log(connectedPeers)
+
+    // Handle data from the connected peer
+    connection.on('data', (data) => {
+        handleIncomingData(data, otherPeerId);
     });
+}
 
-    let connection;
-    // Connect to a peer using their id
-    document.getElementById('connectButton').addEventListener('click', () => {
-        const targetPeer = document.getElementById('peerConnect').value;
-        console.log(targetPeer)
-        connection = peer.connect(targetPeer);
-        console.log(`Connected to: ${targetPeer}`)
-        connection.on('open', () => {
-            connection.send(`${peerId} has connected.`)
-            displayMessage('You', `You are connected to ${targetPeer}`);
-        })
-    })
+// Automatically connect to peer from URL if available
+const otherPeerId = getPeerIdFromURL();
+if (otherPeerId) {
+    connectToPeer(otherPeerId);
+    console.log('connected')
+}
 
-    const sendMessage = () => {
-        if (connection) {
-            console.log(connection)
-            connection.on('open', () => {
-                connection.send(document.getElementById('messageInput').value);
-                displayMessage('You', document.getElementById('messageInput').value);
-                document.getElementById('messageInput').value = '';
-            });
+// Display the list of connected peers
+function updatePeerList() {
+    const peerList = document.getElementById('peerList');
+    peerList.innerHTML = '';
+    for (const peerId in connectedPeers) {
+        const listItem = document.createElement('li');
+        listItem.textContent = peerId;
+        peerList.appendChild(listItem);
+    }
+}
+
+// Handle incoming data (chat messages)
+function handleIncomingData(data, senderPeerId) {
+    if (data.type === 'chat') {
+        const chatMessages = document.getElementById('chatMessages');
+        const messageItem = document.createElement('li');
+        messageItem.textContent = `${senderPeerId}: ${data.message}`;
+        chatMessages.appendChild(messageItem);
+    }
+}
+
+// Send a chat message
+document.getElementById('sendButton').addEventListener('click', () => {
+    const chatInput = document.getElementById('chatInput');
+    const message = chatInput.value.trim();
+    if (message !== '') {
+        const chatData = {
+            type: 'chat',
+            message: message
+        };
+
+        for (const peerId in connectedPeers) {
+            const connection = connectedPeers[peerId];
+            connection.send(chatData);
         }
-    }
 
-    // Send message to connected peer
-    document.getElementById('sendButton').addEventListener('click', () => sendMessage());
-    document.getElementById('messageInput').addEventListener('keyup', (event) => {
-        if (event.key === 'enter') sendMessage();
+        const chatMessages = document.getElementById('chatMessages');
+        const messageItem = document.createElement('li');
+        messageItem.textContent = `You: ${message}`;
+        chatMessages.appendChild(messageItem);
+
+        chatInput.value = '';
+    }
+});
+
+// Listen for connections from other peers
+peer.on('connection', (connection) => {
+    connectedPeers[connection.peer] = connection;
+
+    connection.on('data', (data) => {
+        handleIncomingData(data, connection.peer);
     });
 
-    // Display messages in the chat log
-    const displayMessage = (sender, message) => {
-        const chatLog = document.getElementById('chatLog');
-        const messageDiv = document.createElement('div');
-        messageDiv.innerHTML = `<strong>${sender}:</strong> ${message}`;
-        chatLog.appendChild(messageDiv);
-        chatLog.scrollTop = chatLog.scrollHeight;
-    }
+    connection.on('close', () => {
+        delete connectedPeers[connection.peer];
+        updatePeerList();
+    });
+
+    updatePeerList();
+    console.log(connectedPeers)
 });
